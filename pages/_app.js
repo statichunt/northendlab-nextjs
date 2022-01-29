3a13f8f7dd85e1661319177a14031d00581c4010
tffaruk <faruk.themefisher@gmail.com.com>
2022-01-29 17:55:22 +0600

    customize cookie

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
-import { useState } from "react";
 import "../styles/globals.css";
 import CookieConsent from "react-cookie-consent";
 import Head from "next/head";
 import config from "../config/config.json";
 
 function MyApp({ Component, pageProps }) {
-  const [post, setPost] = useState([]);
   const { perameter } = config;
 
   return (
@@ -13,8 +11,11 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="icon" href={perameter.favicon}></link>
       </Head>
-      <CookieConsent buttonText="Sure man!!" expires={150}>
-        this is coocke
+      <CookieConsent buttonText="Accept" expires={2}>
+        <p>
+          This site uses cookies. By continuing to use this website, you agree
+          to their use.
+        </p>
       </CookieConsent>
       <Component {...pageProps} />
     </>
